1d8fb9154877ea3b8f334db42d043622057f8579
Breno Leal <brenoooleal@gmail.com>
2024-02-16 16:14:50 -0300

    vamo dnv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
             <div className="container">
                 <h2 className="title">Melhores filmes:</h2>
                 <div  className="movies_container">
-                    {topMovies === 0 && <p>Carregando ...</p>}
+                    {topMovies === 0 && <p>Carregando</p>}
                     {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
                 </div>
             </div>
